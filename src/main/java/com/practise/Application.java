616dfd6c754f95aa49e2f992574cb015455752f2
gr-rishad <gr.rishad@gmail.com>
2026-05-03 16:44:31 +0600

    first commit

---
+++ src/main/java/com/practise/Application.java
@@ -0,0 +1,13 @@
+package com.practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from Java Application for Jenkins CI/CD!");
+	}
+
+}
