12484061ba8231df42570358b4e1cf97150db0bb
Golam Rabbani Rishad <33056647+gr-rishad@users.noreply.github.com>
2026-05-04 21:54:39 +0600

    Update comment to clarify webhook functionality

--- src/main/java/com/practise/Application.java
+++ src/main/java/com/practise/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		// comment to check
+		// comment to check is webhook work or not
 		System.out.println("Hello from Java Application for Jenkins CI/CD!");
 	}
 
