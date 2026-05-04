2d0324006c5e0d7c36dd4611f5b03380ffd44e7b
gr-rishad <gr.rishad@gmail.com>
2026-05-04 22:06:59 +0600

    webhook added

--- src/main/java/com/practise/Application.java
+++ src/main/java/com/practise/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		// comment to check is webhook working
+		// comment to check is webhook working or not
 		System.out.println("Hello from Java Application for Jenkins CI/CD!");
 	}
 
